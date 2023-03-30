1375aec93c3f3d1e6eab73e05440c0a4d589fa53
ManuelOliverNacher <manueloliver6@gmail.com>
2023-03-30 19:29:35 -0300

    Proyecto Coder

--- proyecto-oliver-nacher-manuel/src/components/cart/CartContainer.js
+++ proyecto-oliver-nacher-manuel/src/components/cart/CartContainer.js
@@ -6,21 +6,24 @@ import Swal from 'sweetalert2/dist/sweetalert2.js';
 import { FaTrash } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
-
 function CartContainer() {
   const { cart, clearCart, removeProduct } = useContext(cartContext);
   const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
-    let total = 0;
+    setTotalPrice(calculateTotalPrice(cart));
+  }, [cart]);
+
+  function calculateTotalPrice(cart) {
+    let totalPrice = 0;
     for (let i = 0; i < cart.length; i++) {
       const item = cart[i];
       if (item && item.price && !isNaN(parseFloat(item.price))) {
-        total += parseFloat(item.price) * item.quantity;
+        totalPrice += parseFloat(item.price) * item.quantity;
       }
     }
-    setTotalPrice(total);
-  }, [cart]);
+    return totalPrice;
+  }
 
   return (
     <div>
@@ -55,42 +58,25 @@ function CartContainer() {
               className="btn btn-danger mx-5"
               onClick={clearCart}
             >
-            
               Vaciar Carrito
             </button>
           </div>
 
           <div className="mt-3">
-            <h4 className="text-center text-white">
-              Total a Pagar: ${totalPrice.toFixed()}
-            </h4>
-
-            <div className="d-flex justify-content-center mt-4">
-  <Link to="/checkout" className="btn-buy" onClick={() => {
-    Swal.fire({
-      title: "Estas seguro de confirmar la compra?",
-      text: "La compra se realizara!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Si,Confirmar!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire(
-          "Confirmada!",
-          "La compra se realizó con exito.",
-          "success"
-        );
-        window.location.reload();
-      }
-    });
-  }}>
-    Terminar compra
-  </Link>
+  <h4 className="text-center text-white">
+    Total a Pagar: ${totalPrice.toFixed()}
+  </h4>
+  <div className="d-flex justify-content-center mt-4">
+    <Link
+      to="/checkout"
+      className="btn-buy"
+      onClick={() => {
+      }}
+    >
+      Terminar compra
+    </Link>
+  </div>
 </div>
-
-          </div>
         </>
       )}
     </div>
